c243be8eb1109aa487e65f5f1c249565d115b93e
Ketan Vijayvargiya <hi@ketanvijayvargiya.com>
2026-04-10 23:11:05 -0700

    Update

--- flake.nix
+++ flake.nix
@@ -3,10 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-utils.url = "github:numtide/flake-utils";
 
     llm-agents-nix = {
       url = "github:numtide/llm-agents.nix";
